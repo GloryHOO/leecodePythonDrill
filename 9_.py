1199ebebe5c55e2b5c1a805ce5eceff6503ac14c
yangbin <yangbin15@maoyan.com>
2019-05-23 20:46:52 +0800

    13.罗马数字转阿拉伯数字

--- 9_.py
+++ 9_.py
@@ -57,6 +57,19 @@ class Solution:
                 i -= 1
         return roman
 
+    def romanToInt(self, s: str) -> int:
+        romanMap = {'I':1,'IV':4,'V':5,'IX':9,'X':10,'XL':40,'L':50,'XC':90,'C':100, 'CD':400, 'D':500,'CM':900, 'M':1000}
+        pre = 0
+        num = 0
+        for ch in s :
+            i = romanMap[ch]
+            if(i>pre):
+                num = num+i-(pre<<1)
+            else:
+                num += i
+            pre = i
+        return num
+
 
 
 
